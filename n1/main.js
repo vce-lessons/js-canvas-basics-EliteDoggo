4eef5b5e251bb4a0d9177b896a6075aaca2e4a4c
soslan <seelater1337@gmail.com>
2019-09-23 08:11:25 +0300

    w

--- n1/main.js
+++ n1/main.js
@@ -1,1 +1,29 @@
-// Your code here
+let canv = document.createElement('canvas');
+let ctx = canv.getContext('2d');
+
+
+canv.width = window.innerWidth;
+cenv.height = window.innerHeight;
+
+
+document.body.appendChild(canvOne);
+
+
+init();
+
+function init (){
+loop();   
+}
+
+
+function loop (){
+draw();
+requestAnimationFrame(loop);
+
+}
+
+function draw (){
+
+    ctx.fillRect(100,100,160,120);
+
+}
